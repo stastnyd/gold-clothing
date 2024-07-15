7b63f85c4d2bbcd0d4223bf352cfb12143d18e49
Dario <stastnydario@gmail.com>
2024-07-15 22:09:19 +0200

    lint and fixed route

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,17 +1,21 @@
-import { useContext } from 'react';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
-import { CategoriesContext } from '../../contexts/categories.context';
+import { useContext, Fragment } from "react";
 
-const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
-    return (
-        <>
-            {Object.keys(categoriesMap).map((title) => {
-                const products = categoriesMap[title];
-                return <CategoryPreview key={title} products={products} title={title} />;
-            })}
-        </>
-    );
+import { CategoriesContext } from "../../contexts/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </Fragment>
+  );
 };
 
-export default Shop;
+export default CategoriesPreview;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
-import Category from '../category/category.component';
-
-import './shop.styles.scss';
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+import "./shop.styles.scss";
 
 const Shop = () => {
-    return (
-        <Routes>
-            <Route index element={<CategoryPreview />} />
-            <Route path=":category" element={<Category />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
+  );
 };
 
 export default Shop;
