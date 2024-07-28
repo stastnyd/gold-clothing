6b9f5eaa1d7438b7ad82fcdb8c16503eab3e5535
Dario <stastnydario@gmail.com>
2024-07-28 12:00:46 +0200

    linting

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -56,7 +56,6 @@ export const addCollectionAndDocuments = async (
 		batch.set(docRef, object);
 	});
 	await batch.commit();
-	console.log("done saving batch");
 };
 
 export const getCategoriesAndDocuments = async () => {
